08e8b33895a06c46edaf66dd8c71a0382641d82c
Nikita Staroseltsev <nikitozz_13@mail.ru>
2025-01-26 16:46:22 +0300

    Init commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Pages</h1>
+      <Button variant="contained" color="primary">
+        Material-UI Button
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      pages: path.resolve(__dirname, 'src/pages'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+  },
+};
